662e4c555c5e9d95cd69f9df172b7242171d72f4
CatoshiCodr <164091122+CatoshiCodr@users.noreply.github.com>
2025-04-16 18:49:21 +0100

    Add Reactotron Config

--- EgoMoney/src/App.tsx
+++ EgoMoney/src/App.tsx
@@ -4,7 +4,9 @@
  *
  * @format
  */
-
+if (__DEV__) {
+  require('./ReactotronConfig');
+}
 import React from 'react';
 import { Navigation } from './navigation';
 import { Provider} from 'react-redux';
